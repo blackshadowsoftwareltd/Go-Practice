c12c8ee2cd40ffb82722b41a117db5da8ec842da
Remon Ahammad <blackshadowsoftwareltd@gmail.com>
2022-04-02 01:04:01 +0600

    second day

--- booking-app/main.go
+++ booking-app/main.go
@@ -8,31 +8,44 @@ func main() {
 	conferenceName := "Go Conference"
 	const conferenceTickets int = 50
 	var remainingTickets uint = uint(conferenceTickets)
+	var bokings []string
 
 	fmt.Printf("conferenceTickets %T remainingTickets is %T, conferenceName is %T\n", conferenceTickets, remainingTickets, conferenceName)
 
 	fmt.Printf("Welcome to %v booking application\n", conferenceName)
 	fmt.Printf("We have total of %v tickets and %v are still available\n", conferenceTickets, remainingTickets)
 	fmt.Println("Get your tickets here to attend")
+	for {
 
-	var firlstName, lastName, email string
-	var userTickets uint
-	// ask user for their name
+		var firstName, lastName string
+		var userTickets uint
+		// ask user for their name
 
-	fmt.Println("Enter your email : ")
-	fmt.Scan(&email)
+		fmt.Println("Enter your first name : ")
+		fmt.Scan(&firstName)
 
-	fmt.Println("Enter your first name : ")
-	fmt.Scan(&firlstName)
+		fmt.Println("Enter your last name : ")
+		fmt.Scan(&lastName)
 
-	fmt.Println("Enter your last name : ")
-	fmt.Scan(&lastName)
+		fmt.Println("Enter numger of tickets : ")
+		fmt.Scan(&userTickets)
+		if userTickets <= remainingTickets {
 
-	fmt.Println("Enter numger of tickets : ")
-	fmt.Scan(&userTickets)
+			remainingTickets = remainingTickets - userTickets
+			bokings = append(bokings, firstName+" "+lastName)
 
-	remainingTickets = remainingTickets - userTickets
+			fmt.Printf("Thank you %v %v for booking %v tickets. You will receive a confirmation email \n", firstName, lastName, userTickets)
+			fmt.Printf("%v tickets remaining for %v\n", remainingTickets, conferenceName)
 
-	fmt.Printf("Thank you %v %v for booking %v tickets. You will receive a confirmation email at %v\n", firlstName, lastName, userTickets, email)
-	fmt.Printf("%v tickets remaining for %v",remainingTickets,conferenceName)
+			if remainingTickets == 0 {
+				// end program
+				fmt.Printf("Our conference is booked out. Come back next year!\n")
+				break
+			}
+		} else {
+			fmt.Printf("We onley have %v tickets remaining, so you can't book %v tickets\n", remainingTickets, userTickets)
+
+		}
+
+	}
 }

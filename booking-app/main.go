5a1d7cfbd4b415e37eb24c52c7fa1b1f0cb6db5d
remon <blackshadowsoftwareltd@gmail.com>
2022-04-06 01:47:38 +0600

    fourth day

--- booking-app/main.go
+++ booking-app/main.go
@@ -10,29 +10,17 @@ func main() {
 	const conferenceTickets int = 50
 	var remainingTickets uint = uint(conferenceTickets)
 	var bokings []string
+	greetUsers(conferenceName, conferenceTickets, remainingTickets)
 
-	fmt.Printf("conferenceTickets %T remainingTickets is %T, conferenceName is %T\n", conferenceTickets, remainingTickets, conferenceName)
-
-	fmt.Printf("Welcome to %v booking application\n", conferenceName)
-	fmt.Printf("We have total of %v tickets and %v are still available\n", conferenceTickets, remainingTickets)
-	fmt.Println("Get your tickets here to attend")
 	for {
 
 		var firstName, lastName, email string
 		var userTickets uint
 		// ask user for their name
 
-		fmt.Println("Enter your Email : ")
-		fmt.Scan(&email)
-
-		fmt.Println("Enter your first name : ")
-		fmt.Scan(&firstName)
-
-		fmt.Println("Enter your last name : ")
-		fmt.Scan(&lastName)
-
-		fmt.Println("Enter numger of tickets : ")
-		fmt.Scan(&userTickets)
+		email = pickUserEmail(email)
+		firstName, lastName = pickFirstAndLastName(firstName,lastName)
+		userTickets = pickUserTickets(userTickets)
 
 		isValidName := len(firstName) >= 3 && len(lastName) >= 3
 		isValidEmail := strings.Contains(email, "@")
@@ -58,4 +46,30 @@ func main() {
 		}
 
 	}
+}
+func pickUserTickets(userTickets uint) uint {
+	fmt.Println("Enter numger of tickets : ")
+	fmt.Scan(&userTickets)
+	return userTickets
+}
+
+func pickFirstAndLastName(firstName, lastName string) (string, string) {
+	fmt.Println("Enter your first name : ")
+	fmt.Scan(&firstName)
+
+	fmt.Println("Enter your last name : ")
+	fmt.Scan(&lastName)
+	return firstName, lastName
+}
+func pickUserEmail(email string) string {
+	fmt.Println("Enter your Email : ")
+	fmt.Scan(&email)
+	return email
+
+}
+
+func greetUsers(conferenceName string, conferenceTickets int, remainingTickets uint) {
+	fmt.Printf("Welcome to %v booking application\n", conferenceName)
+	fmt.Printf("We have total of %v tickets and %v are still available\n", conferenceTickets, remainingTickets)
+	fmt.Println("Get your tickets here to attend")
 }

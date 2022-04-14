9a546d484729feb8a68ffd0cbe435e05f83e73bc
remon <blackshadowsoftwareltd@gmail.com>
2022-04-14 15:57:02 +0600

    struct

--- Practice/main.go
+++ Practice/main.go
@@ -5,11 +5,12 @@ import (
 )
 
 func main() {
-	var data Objects = Objects{"Remon", 22}
-	fmt.Println("Name : ", data.name, "\nAge : ", data.age)
+	var data Objects = Objects{"Remon", "", 22}
+	data.lastName = "Ahammad"
+	fmt.Println("First name : ", data.firstName, "\nLast name : ", data.lastName, "\nAge : ", data.age)
 }
 
 type Objects struct {
-	name string
-	age  int
+	firstName, lastName string
+	age                 int
 }

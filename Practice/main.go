da0a4ec48e897d8c6ea905ba0bf97a928403af13
blackshadow <blackshadowsoftwareltd@gmail.com>
2022-04-29 23:21:01 +0600

    pointer. change value using pointer

--- Practice/main.go
+++ Practice/main.go
@@ -1,8 +1,21 @@
 package main
 
-import ( 
+import (
+	"fmt"
 )
 
 func main() {
-doSomeThing()
+	name := "Remon"
+	address := &name //? for getting memory address. use &
+
+	fmt.Println("memory address of name: ", address)
+	fmt.Println("value of memory address: ", *address) //? for getting value of memory address. use *
+
+	fmt.Println(name)
+	changValueUsingPointer(address)
+	fmt.Println(name)
+}
+
+func changValueUsingPointer(x *string) { //? for changing value usint pointer use * before data type and data
+	*x = "Remon Ahammad"
 }

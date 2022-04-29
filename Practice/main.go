38b6c2be91ab4956febd8eba7c709931884f7ad8
blackshadow <blackshadowsoftwareltd@gmail.com>
2022-04-29 21:36:31 +0600

    sort pakcage

--- Practice/main.go
+++ Practice/main.go
@@ -2,15 +2,21 @@ package main
 
 import (
 	"fmt"
-	"strings"
+	"sort"
 )
 
 func main() {
+	_list := []int{5, 6, 7, 1, 2, 3, 10, 4, 8, 9}
+	_datas := []string{"g", "h", "a", "b", "e", "f", "c", "j", "d", "i"}
+	fmt.Println(_list)
+	fmt.Println(_datas)
 
-	data := "Hello World World"
-	fmt.Println(strings.ToUpper(data))
-	fmt.Println(strings.ToLower(data))
-	fmt.Println(strings.Index(data,"W"))
-	fmt.Println(strings.Split(data," "))
+	sort.Ints(_list)     //? sort
+	sort.Strings(_datas) //? sort
 
+	fmt.Println(_list)
+	fmt.Println(_datas)
+
+	fmt.Println(sort.SearchInts(_list, 10))      //? index of list's index
+	fmt.Println(sort.SearchStrings(_datas, "d")) //? index of list's index
 }

6732c951bba1c583360c55731e43d1848b8e0ee3
blackshadow <blackshadowsoftwareltd@gmail.com>
2022-05-05 14:38:27 +0600

    clear all

--- Practice/main.go
+++ Practice/main.go
@@ -1,56 +1,7 @@
 package main
 
-import (
-	"fmt"
-	"math"
-)
+import ()
 
 func main() {
-	shapes := []shape{
-		square{length: 15.2},
-		circle{radius: 7.5},
-		circle{radius: 12.3},
-		square{length: 4.9},
-	}
 
-	for _, v := range shapes {
-		printShapeInfo(v)
-		println("---------------")
-	}
-}
-
-//? shape interface
-type shape interface {
-	area() float64
-	circumf() float64
-}
-type square struct {
-	length float64
-}
-type circle struct {
-	radius float64
-}
-
-//? square methods
-func (s square) area() float64 {
-	return s.length * s.length
-}
-
-func (s square) circumf() float64 {
-	return s.length * 4
-}
-
-//? circle methods
-func (c circle) area() float64 {
-	return math.Pi * c.radius
-}
-
-func (c circle) circumf() float64 {
-	return 2 * math.Pi * c.radius
-}
-
-//? print
-func printShapeInfo(s shape) {
-	fmt.Printf("area of %T is: %0.2f\n", s, s.area())
-	fmt.Printf("circumference of %T is: %0.2f \n", s, s.circumf())
 }

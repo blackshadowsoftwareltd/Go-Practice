c8eecbb730eb14960ab38d4545e110104af9fbdc
remon <blackshadowsoftwareltd@gmail.com>
2022-07-16 01:45:54 +0600

    -

--- Practice/main.go
+++ Practice/main.go
@@ -6,47 +6,39 @@ import (
 )
 
 func main() {
-	data := "Hello people. I love you all. And I also love dart & golang. dart.io & golang.org"
-	list := strings.Split(data, ".")
-	ext := list[len(list)-1] //? for getting the last element of the slice
-	fmt.Println(ext)
-	fmt.Println(list[2]) //? for getting the spacific element of the slice
+	var users []User = []User{
+		{"John", 20},
+		{"Jane", 21},
+		{"Jill", 23},
+		{"Joe", 24},
+		{"Jack", 22},
+	}
+	fmt.Println(users)
+
+	///? searching in slice by name (raw way)
+	var newUsers []User
+	for _, u := range users {
+		if strings.Contains(strings.ToLower(u.Name), strings.ToLower("k")) {
+			newUsers = append(newUsers, u)
+		}
+	}
+	fmt.Println(newUsers)
+	///? is contsins in a slice
+	fmt.Println("is contains", isContainsInSlice(users, "x"))
 }
 
-// func main() {
-// 	var users []User = []User{
-// 		{"John", 20},
-// 		{"Jane", 21},
-// 		{"Jill", 23},
-// 		{"Joe", 24},
-// 		{"Jack", 22},
-// 	}
-// 	fmt.Println(users)
+///? is contains in a slice
+func isContainsInSlice(s []User, name string) bool {
+	for _, u := range s {
+		if strings.Contains(strings.ToLower(u.Name), strings.ToLower(name)) {
+			return true
+		}
+	}
+	return false
 
-// 	///? searching in slice by name (raw way)
-// 	var newUsers []User
-// 	for _, u := range users {
-// 		if strings.Contains(strings.ToLower(u.Name), strings.ToLower("k")) {
-// 			newUsers = append(newUsers, u)
-// 		}
-// 	}
-// 	fmt.Println(newUsers)
-// 	///? is contsins in a slice
-// 	fmt.Println("is contains", isContainsInSlice(users, "x"))
-// }
+}
 
-// ///? is contains in a slice
-// func isContainsInSlice(s []User, name string) bool {
-// 	for _, u := range s {
-// 		if strings.Contains(strings.ToLower(u.Name), strings.ToLower(name)) {
-// 			return true
-// 		}
-// 	}
-// 	return false
-
-// }
-
-// type User struct {
-// 	Name string
-// 	Age  int
-// }
+type User struct {
+	Name string
+	Age  int
+}

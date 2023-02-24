ea02d95e30ae45fde1e43977a8ec113c4ba8acd2
remon <blackshadowsoftwareltd@gmail.com>
2023-02-24 23:06:42 +0600

    debuging with switch case statement

--- Practice/main.go
+++ Practice/main.go
@@ -9,18 +9,19 @@ func main() {
 	fmt.Println(time.Now().UTC().Local())
 	x := 5
 
-	if x == 1 {
-		fmt.Println("x is 1")
-	} else if x == 2 {
-		fmt.Println("x is 2")
-	} else if x == 3 {
-		fmt.Println("x is 3")
-	} else if x == 4 {
-		fmt.Println("x is 4")
-	} else if x == 5 {
-		fmt.Println("x is 5")
-	} else {
-		fmt.Println("x is not found")
+	switch x {
+	case 1:
+		fmt.Println("one")
+	case 2:
+		fmt.Println("two")
+	case 3:
+		fmt.Println("three")
+	case 4:
+		fmt.Println("four")
+	case 5:
+		fmt.Println("five")
+	default:
+		fmt.Println("default")
 	}
 	fmt.Println(time.Now().UTC().Local())
 }
